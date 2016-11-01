d9ba45d8b5393e3d1f5356e18aa6117fe65cf183
David Almeida (Soyun) <david.almeida@soyun.es>
2016-11-01 09:53:33 +0100

    added scsslint disable for copied scss files

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,11 @@ var devTools = require('syn-dev-tools').gulp
 var manager = devTools.Manager.getInstance(gulp)
 manager.registerTasks()
 
+var insert = require('gulp-insert')
 gulp.task('vendor-scss-copy', function () {
   return gulp.src([
     'node_modules/sweetalert/dev/sweetalert.scss'
   ])
+    .pipe(insert.prepend('// scss-lint:disable all\n'))
     .pipe(gulp.dest('src/scss/vendor'))
 })
