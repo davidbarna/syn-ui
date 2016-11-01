175e20ebb19c0f4b351b7af516b32be4776086d9
David Almeida (Soyun) <david.almeida@soyun.es>
2016-11-01 09:49:11 +0100

    Dependencies's scss copied to scss folder
    It’s impossible to manager submodules scss inclusion with syn-dev-tools

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
  * Gulpfile
  * Tasks are registered from dev-tools module.
  */
+var gulp = require('gulp')
 var devTools = require('syn-dev-tools').gulp
-var manager = devTools.Manager.getInstance(require('gulp'))
+var manager = devTools.Manager.getInstance(gulp)
 manager.registerTasks()
+
+gulp.task('vendor-scss-copy', function () {
+  return gulp.src([
+    'node_modules/sweetalert/dev/sweetalert.scss'
+  ])
+    .pipe(gulp.dest('src/scss/vendor'))
+})
